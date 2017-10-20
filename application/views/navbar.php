ff003204f75c26f514ca728e7069d070c46b02d1
pablee <tcpipablo@gmail.com>
2017-10-20 17:13:04 -0300

    Navbar

--- application/views/navbar.php
+++ application/views/navbar.php
@@ -5,7 +5,51 @@
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">Motos</a>
                     <ul class="dropdown-menu" role="menu">
-
+                        <div class="row">
+                            <div class="col-md-1 margin-top">
+                            </div>
+                            <div class="col-md-2 margin-top">
+                                <li><b>Tipo</b></li>
+                                <li><a href="#">Abiertos</a></li>
+                                <li><a href="#">Motocross</a></li>
+                                <li><a href="#">Integrales</a></li>
+                                <li><a href="#">Rebatibles</a></li>
+                                <li><a href="#">Modulares</a></li>
+                            </div>
+                            <div class="col-md-1 margin-top">
+                                <li><b>Marcas</b></li>
+                                <li>Acerbis</li>
+                                <li>Agv</li>
+                                <li>Airoh</li>
+                                <li>Arai</li>
+                            </div>
+                            <div class="col-md-1 margin-top">
+                                <br>
+                                <li>Benelli</li>
+                                <li>Beon</li>
+                                <li>Fox</li>
+                                <li>Givi</li>
+                                <li>Hjc</li>
+                            </div>
+                            <div class="col-md-1 margin-top">
+                                <br>
+                                <li>Ls2</li>
+                                <li>Momo</li>
+                                <li>Nzi</li>
+                                <li>Origine</li>
+                                <li>Peels</li>
+                            </div>
+                            <div class="col-md-1 margin-top">
+                                <br>
+                                <li>Schuberth</li>
+                                <li>Shiro</li>
+                                <li>Shoei</li>
+                                <li>V-can</li>
+                            </div>
+                            <div class="col-md-5 text-right">
+                                <img src="<?php echo base_url(); ?>assets/img/categorias/cat_casco.jpg" alt="casco" width="300" height="300">
+                            </div>
+                        </div>
                     </ul>
                 </li>
                 <li class="dropdown">
@@ -22,13 +66,101 @@
                                 <li><a href="#">Rebatibles</a></li>
                                 <li><a href="#">Modulares</a></li>
                             </div>
-                            <div class="col-md-2 margin-top">
+                            <div class="col-md-1 margin-top">
                                 <li><b>Marcas</b></li>
-                                <li><a href="#">Jet</a></li>
-                                <li><a href="#">Shoei</a></li>
-                                <li><a href="#">Agv</a></li>
-                                <li><a href="#">Hjc</a></li>
-                                <li><a href="#">Arai</a></li>
+                                <li>Acerbis</li>
+                                <li>Agv</li>
+                                <li>Airoh</li>
+                                <li>Arai</li>
+                            </div>
+                            <div class="col-md-1 margin-top">
+                                <br>
+                                <li>Benelli</li>
+                                <li>Beon</li>
+                                <li>Fox</li>
+                                <li>Givi</li>
+                                <li>Hjc</li>
+                            </div>
+                            <div class="col-md-1 margin-top">
+                                <br>
+                                <li>Ls2</li>
+                                <li>Momo</li>
+                                <li>Nzi</li>
+                                <li>Origine</li>
+                                <li>Peels</li>
+                            </div>
+                            <div class="col-md-1 margin-top">
+                                <br>
+                                <li>Schuberth</li>
+                                <li>Shiro</li>
+                                <li>Shoei</li>
+                                <li>V-can</li>
+                            </div>
+                            <div class="col-md-5 text-right">
+                                <img src="<?php echo base_url(); ?>assets/img/categorias/cat_casco.jpg" alt="casco" width="300" height="300">
+                            </div>
+
+                        </div>
+                    </ul>
+                </li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Indumentaria</a>
+                    <ul class="dropdown-menu" role="menu">
+                        <div class="row">
+                            <div class="col-md-1 margin-top">
+                            </div>
+                            <div class="col-md-2 margin-top">
+                                <li><b>Tipo</b></li>
+                                <li><a href="#">Pista</a></li>
+                                <li><a href="#">Touring</a></li>
+                                <li><a href="#">Enduro-cross</a></li>
+                            </div>
+                            <div class="col-md-1 margin-top">
+                                <li><b>Marcas</b></li>
+                                <li>Acerbis</li>
+                                <li>Fox</li>
+                                <li>Joe rocket</li>
+                                <li>Motorman</li>
+                            </div>
+                            <div class="col-md-1 margin-top">
+                                <br>
+                                <li>Octane</li>
+                                <li>Rpmcroos</li>
+                                <li>Shift</li>
+                                <li>Yamaha</li>
+                            </div>
+
+                            <div class="col-md-7 text-right">
+                                <img src="<?php echo base_url(); ?>assets/img/categorias/cat_casco.jpg" alt="casco" width="300" height="300">
+                            </div>
+
+                        </div>
+                    </ul>
+                </li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Calzado</a>
+                    <ul class="dropdown-menu" role="menu">
+                        <div class="row">
+                            <div class="col-md-1 margin-top">
+                            </div>
+                            <div class="col-md-2 margin-top">
+                                <li><b>Tipo</b></li>
+                                <li><a href="#">Pista</a></li>
+                                <li><a href="#">Motocross</a></li>
+                                <li><a href="#">Touring</a></li>
+                                <li><a href="#">Enduro-cross</a></li>
+                            </div>
+                            <div class="col-md-1 margin-top">
+                                <li><b>Marcas</b></li>
+                                <li>Acerbis</li>
+                                <li>Cros Roads</li>
+                                <li>Forma</li>
+                            </div>
+                            <div class="col-md-1 margin-top">
+                                <br>
+                                <li>Fox</li>
+                                <li>Gaerne</li>
+                                <li>Sidi</li>
                             </div>
                             <div class="col-md-7 text-right">
                                 <img src="<?php echo base_url(); ?>assets/img/categorias/cat_casco.jpg" alt="casco" width="300" height="300">
@@ -37,14 +169,183 @@
                         </div>
                     </ul>
                 </li>
-                <li><a href="#">Indumentaria</a></li>
-                <li><a href="#">Calzado</a></li>
-                <li><a href="#">Accesorios</a></li>
-                <li><a href="#">Cubiertas</a></li>
-                <li><a href="#">Repuestos</a></li>
-                <li><a href="#">Seguridad</a></li>
-                <li><a href="#">Protecciones</a></li>
-                <li><a href="#">Conocenos</a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Accesorios</a>
+                    <ul class="dropdown-menu" role="menu">
+                        <div class="row">
+                            <div class="col-md-1 margin-top">
+                            </div>
+                            <div class="col-md-2 margin-top">
+                                <li><b>Tipo</b></li>
+                                <li><a href="#">Antiparras</a></li>
+                                <li><a href="#">Bauleras</a></li>
+                                <li><a href="#">Intercomunicadores</a></li>
+                            </div>
+                            <div class="col-md-1 margin-top">
+                                <li><b>Marcas</b></li>
+                                <li>Coocase</li>
+                                <li>Ethen</li>
+                                <li>Fox</li>
+                                <li>Givi</li>
+                                <li>Nenki</li>
+                            </div>
+                            <div class="col-md-1 margin-top">
+                                <br>
+                                <li>Progrip</li>
+                                <li>Sena</li>
+                                <li>Shad</li>
+                                <li>Thor</li>
+                                <li>Vega</li>
+                            </div>
+                            <div class="col-md-7 text-right">
+                                <img src="<?php echo base_url(); ?>assets/img/categorias/cat_casco.jpg" alt="casco" width="300" height="300">
+                            </div>
+
+                        </div>
+                    </ul>
+                </li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Cubiertas</a>
+                    <ul class="dropdown-menu" role="menu">
+                        <div class="row">
+                            <div class="col-md-1 margin-top">
+                            </div>
+                            <div class="col-md-2 margin-top">
+                                <li><b>Tipo</b></li>
+                                <li><a href="#">Motocross</a></li>
+                                <li><a href="#">Cuatri</a></li>
+                                <li><a href="#">Enduro calle</a></li>
+                            </div>
+                            <div class="col-md-2 margin-top">
+                                <li><b>Marcas</b></li>
+                                <li>Continental</li>
+                                <li>Dunlop</li>
+                                <li>Metzeler</li>
+                                <li>Pirelli</li>
+                                <li>Rinaldi</li>
+                                <li>Torc</li>
+                            </div>
+
+                            <div class="col-md-7 text-right">
+                                <img src="<?php echo base_url(); ?>assets/img/categorias/cat_casco.jpg" alt="casco" width="300" height="300">
+                            </div>
+
+                        </div>
+                    </ul>
+                </li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Repuestos</a>
+                    <ul class="dropdown-menu" role="menu">
+                        <div class="row">
+                            <div class="col-md-1 margin-top">
+                            </div>
+                            <div class="col-md-2 margin-top">
+                                <li><b>Tipo</b></li>
+                                <li><a href="#">Baterias</a></li>
+                                <li><a href="#">Escapes</a></li>
+                            </div>
+                            <div class="col-md-1 margin-top">
+                                <li><b>Marcas</b></li>
+                                <li>Cott</li>
+                                <li>Dinavolt</li>
+                                <li>Dirt Race</li>
+                            </div>
+                            <div class="col-md-1 margin-top">
+                                <br>
+                                <li>Maxim</li>
+                                <li>Outdo</li>
+                                <li>R72</li>
+                            </div>
+                            <div class="col-md-7 text-right">
+                                <img src="<?php echo base_url(); ?>assets/img/categorias/cat_casco.jpg" alt="casco" width="300" height="300">
+                            </div>
+
+                        </div>
+                    </ul>
+                </li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Seguridad</a>
+                    <ul class="dropdown-menu" role="menu">
+                        <div class="row">
+                            <div class="col-md-1 margin-top">
+                            </div>
+                            <div class="col-md-2 margin-top">
+                                <li><b>Tipo</b></li>
+                                <li><a href="#">Alarmas</a></li>
+                                <li><a href="#">Candados</a></li>
+                                <li><a href="#">Lingas</a></li>
+                                <li><a href="#">Trabadiscos</a></li>
+                            </div>
+                            <div class="col-md-1 margin-top">
+                                <li><b>Marcas</b></li>
+                                <li>Kovix</li>
+                                <li>Plaza</li>
+                                <li>Positron</li>
+                            </div>
+
+                            <div class="col-md-8 text-right">
+                                <img src="<?php echo base_url(); ?>assets/img/categorias/cat_casco.jpg" alt="casco" width="300" height="300">
+                            </div>
+                        </div>
+                    </ul>
+                </li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Protecciones</a>
+                    <ul class="dropdown-menu" role="menu">
+                        <div class="row">
+                            <div class="col-md-1 margin-top">
+                            </div>
+                            <div class="col-md-2 margin-top">
+                                <li><b>Tipo</b></li>
+                                <li><a href="#">Coderas</a></li>
+                                <li><a href="#">Integrales</a></li>
+                                <li><a href="#">Pecheras</a></li>
+                                <li><a href="#">Rodilleras</a></li>
+                            </div>
+                            <div class="col-md-1 margin-top">
+                                <li><b>Marcas</b></li>
+                                <li>Acerbis</li>
+                                <li>Fox</li>
+                                <li>Shift</li>
+                            </div>
+                            <div class="col-md-8 text-right">
+                                <img src="<?php echo base_url(); ?>assets/img/categorias/cat_casco.jpg" alt="casco" width="300" height="300">
+                            </div>
+                        </div>
+                    </ul>
+                </li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Conocenos</a>
+                    <ul class="dropdown-menu" role="menu">
+                        <div class="row">
+                            <div class="col-md-1 margin-top">
+                            </div>
+                            <div class="col-md-2 margin-top">
+                                <li><b>Tipo</b></li>
+                                <li><a href="#">Pista</a></li>
+                                <li><a href="#">Motocross</a></li>
+                                <li><a href="#">Touring</a></li>
+                                <li><a href="#">Enduro-cross</a></li>
+                            </div>
+                            <div class="col-md-1 margin-top">
+                                <li><b>Marcas</b></li>
+                                <li>Acerbis</li>
+                                <li>Cros Roads</li>
+                                <li>Forma</li>
+                            </div>
+                            <div class="col-md-1 margin-top">
+                                <br>
+                                <li>Fox</li>
+                                <li>Gaerne</li>
+                                <li>Sidi</li>
+                            </div>
+                            <div class="col-md-7 text-right">
+                                <img src="<?php echo base_url(); ?>assets/img/categorias/cat_casco.jpg" alt="casco" width="300" height="300">
+                            </div>
+
+                        </div>
+                    </ul>
+                </li>
             </ul>
         </div>
     </nav>
