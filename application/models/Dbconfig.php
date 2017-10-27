be90e5e02a80ed8a1275c7d415d608869aff9367
pablee <tcpipablo@gmail.com>
2017-10-27 14:08:00 -0300

    Configuracion servidor

--- application/models/Dbconfig.php
+++ application/models/Dbconfig.php
@@ -1,9 +1,16 @@
 <?php
-//define	("server","127.0.0.1");
+/*Servidor*/
+/*
+define	("server","10.0.10.42");
+define	("user","vj4658458_admin");
+define	("password","Lavalle123");
+define	("database","vj4658458_lavalle");
+*/
+
+//Desarrollo
 define	("server","localhost");
 define	("user","root");
 define	("password","");
 define	("database","lavalle");
-//echo server . "<br>" . user . "<br>" . password . "<br>" . database . "<br>";
 
 ?>
