be7f84469d68b3eb97f3a1b4f1198f9fd5f3bb10
pablee <tcpipablo@gmail.com>
2017-11-01 08:46:56 -0300

    mobile contacto

--- application/views/contacto.php
+++ application/views/contacto.php
@@ -1,10 +1,9 @@
 <body class="container-fluid">
 
-
     <!--div class="container-fluid"-->
         <!--Informacion de contacto en el header-->
-        <div class="row text-center info_contacto_header" id="info_contacto_header">
-            <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="row hidden-xs hidden-sm text-center info_contacto_header" id="info_contacto_header">
+            <div class="col-md-12">
                 <span style="margin-right: 25px;">
                     <span style="color:#FF0000; padding-right: 3px" class="glyphicon glyphicon-envelope"></span><a href="#" data-toggle="modal" data-target="#contacto"> Escribinos </a>
                 </span>
