b1f5d8d531265f8b78af672dfa8b36445734f441
pablee <tcpipablo@gmail.com>
2017-12-21 08:49:27 -0300

    Configuracion config.php y dbconfig

--- application/config/config.php
+++ application/config/config.php
@@ -24,6 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 //$config['base_url'] = 'http://viajandojuntos.com.ar/';
+//$config['base_url'] = 'http://lavallemotos.com.ar/prueba';
 $config['base_url'] = '';
 /*
 |--------------------------------------------------------------------------
--- application/models/Dbconfig.php
+++ application/models/Dbconfig.php
@@ -1,10 +1,19 @@
 <?php
-/*Servidor*/
+/*Servidor viajandojuntos*/
 /*
 define	("server","10.0.10.42");
 define	("user","vj4658458_admin");
 define	("password","Lavalle123");
 define	("database","vj4658458_lavalle");
+*/
+
+
+/*Lavalle prueba*/
+/*
+define	("server","lavallemotos.com.ar");
+define	("user","ecommerce");
+define	("password","Ll12345");
+define	("database","lavalledb_ecommerce");
 */
 
 //Desarrollo
